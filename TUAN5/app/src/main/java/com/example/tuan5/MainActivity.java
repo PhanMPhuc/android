2c75bd9fe5b20770034312c4a5d59b8fe2205f97
PhanMPhuc <phanminhphuc05@gmail.com>
2025-09-26 23:03:50 +0700

    TUAN5 - fourth push
    Gan click cho ScreenA va ScreenB

--- TUAN5/app/src/main/java/com/example/tuan5/MainActivity.java
+++ TUAN5/app/src/main/java/com/example/tuan5/MainActivity.java
@@ -1,24 +1,39 @@
 package com.example.tuan5;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Tìm 2 nút trong layout
+        Button btnA = findViewById(R.id.btn_screen_a);
+        Button btnB = findViewById(R.id.btn_screen_b);
+
+        // Gán sự kiện click cho nút A
+        btnA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ScreenAActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Gán sự kiện click cho nút B
+        btnB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ScreenBActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
